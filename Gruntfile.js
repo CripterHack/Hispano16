36634afd3ac589a822ef9cd3fcdb171fa6a892af
CripterHack <edgar.zorrilla@openmailbox.org>
2016-02-26 14:15:21 -0600

    All Update 4

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,10 +127,12 @@ module.exports = function(grunt){
  grunt.loadNpmTasks('grunt-contrib-htmlmin');
  grunt.loadNpmTasks('grunt-contrib-watch');
  grunt.loadNpmTasks('grunt-contrib-jshint');
+ grunt.loadNpmTasks('grunt-git');
  grunt.registerTask('default', [
     'concat',
     'uglify',
     'cssmin',
-    'autoprefixer'
+    'autoprefixer',
+    'grunt-git'
   ]);
 };
